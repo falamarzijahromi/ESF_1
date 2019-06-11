a440488d8f3b20cb656ad7401a1cb0d2bef680af
MehadiFalamazi <falamarzijahromi.mm@gmail.com>
2019-06-11 23:29:15 +0430

    Added factory delegate for service types

--- Source/Composition.ESF_1/IIocContainer.cs
+++ Source/Composition.ESF_1/IIocContainer.cs
@@ -5,5 +5,7 @@ namespace Composition.ESF_1
     public interface IIocContainer
     {
         void RegisterPerGraph(Type[] serviceTypes, Type implementationType, Type[] interceptorTypes = null);
+
+        void RegisterFactory(Type[] serviceTypes, Func<object> serviceFactory, Type[] interceptors = null);
     }
 }
