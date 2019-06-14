3e379100cee518c4981b3adaad281a2b77671af7
MehadiFalamazi <falamarzijahromi.mm@gmail.com>
2019-06-14 22:33:51 +0430

    TransactionInterceptor changed to UnitOfWorkInterceptor, TransactionInterceptor created for Transactionscope support feature.

--- Source/Interceptors.ESF_1/TransactionInterceptor.cs
+++ Source/Interceptors.ESF_1/TransactionInterceptor.cs
@@ -1,36 +1,18 @@
-﻿using Composition.ESF_1;
-using UnitOfWork.ESF_1;
+﻿using System.Transactions;
+using Composition.ESF_1;
 
 namespace Interceptors.ESF_1
 {
     public class TransactionInterceptor : IServiceInterceptor
     {
-        private readonly IUnitOfWork unitOfWork;
-
-        public TransactionInterceptor(IUnitOfWork unitOfWork)
-        {
-            this.unitOfWork = unitOfWork;
-        }
-
         public void Intercept(IInterceptionContext interceptionContext)
         {
-            if (!unitOfWork.IsStarted)
-            {
-                unitOfWork.Begin();
-            }
-
-            try
+            using (var trxScope = new TransactionScope())
             {
                 interceptionContext.Proceed();
-            }
-            catch
-            {
-                unitOfWork.Rollback();
 
-                throw;
+                trxScope.Complete();
             }
-
-            unitOfWork.Commit();
         }
     }
 }
---
+++ Source/Interceptors.ESF_1/UnitOfWorkInterceptor.cs
@@ -0,0 +1,36 @@
+﻿using Composition.ESF_1;
+using UnitOfWork.ESF_1;
+
+namespace Interceptors.ESF_1
+{
+    public class UnitOfWorkInterceptor : IServiceInterceptor
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public UnitOfWorkInterceptor(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public void Intercept(IInterceptionContext interceptionContext)
+        {
+            if (!unitOfWork.IsStarted)
+            {
+                unitOfWork.Begin();
+            }
+
+            try
+            {
+                interceptionContext.Proceed();
+            }
+            catch
+            {
+                unitOfWork.Rollback();
+
+                throw;
+            }
+
+            unitOfWork.Commit();
+        }
+    }
+}
