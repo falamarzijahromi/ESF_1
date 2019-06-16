79761a25c8bae1a7e5456eb1ec253d2e56fd51b8
MehadiFalamazi <falamarzijahromi.mm@gmail.com>
2019-06-16 11:30:22 +0430

    Added factory per graph feature.

--- Source/Composition.ESF_1/IIocContainer.cs
+++ Source/Composition.ESF_1/IIocContainer.cs
@@ -7,5 +7,7 @@ namespace Composition.ESF_1
         void RegisterPerGraph(Type[] serviceTypes, Type implementationType, Type[] interceptorTypes = null);
 
         void RegisterFactoryTransient(Type[] serviceTypes, Func<IResolver, object> serviceFactory, Type[] interceptors = null);
+
+        void RegisterFactoryPerGraph(Type[] serviceTypes, Func<IResolver, object> serviceFactory, Type[] interceptors = null);
     }
 }
